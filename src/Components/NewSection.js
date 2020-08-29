29331ab0aca3619c03396d2e4507a2de68125c78
Juanma Megia <jma.megia@gmail.com>
2020-08-29 20:05:34 +0200

    fix add section button animation

--- src/Components/NewSection.js
+++ src/Components/NewSection.js
@@ -6,22 +6,29 @@ import "../CSS/newSection.css";
 
 function Construction() {
   const context = useContext(Context);
+  const [sectionForm, setSectionForm] = useState(false);
+  const [name, setName] = useState("");
   const toggleSectionForm = (e) => {
+    e.stopPropagation();
     e.preventDefault();
-    setSectionForm(!sectionForm);
+    setSectionForm(false);
   };
   const sendForm = (e) => {
     e.preventDefault();
+    e.stopPropagation();
+    setSectionForm(false);
     services.updateSection({ name }, context.token);
   };
-  const [sectionForm, setSectionForm] = useState(false);
-  const [name, setName] = useState("");
   const onChangeHandler = (e) => {
     setName(e.target.value);
   };
   return (
-    <div className="newSection" onClick={toggleSectionForm}>
-      <img className="addIcon" src={addIcon} alt="add" />
+    <div
+      className={`newSection ${sectionForm ? "showingForm" : "hiddingForm"}`}
+      onClick={() => setSectionForm(true)}
+    >
+      <img className={`addIcon`} src={addIcon} alt="add" />
+
       <form className={`form ${sectionForm ? "fshow" : "fhide"}`}>
         <div>
           <label htmlFor="name">
